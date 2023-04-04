e6b0c7c456e70b1124ff6b5fa9176c180f7b3ae9
Mark Strempel <mark.strempel@codecool.com>
2023-04-04 10:37:25 +0200

    NoteList component shows all the notes.

--- src/main/js/NoteList.jsx
+++ src/main/js/NoteList.jsx
@@ -1,10 +1,30 @@
-import { useParams } from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+import api from "./api.js";
 
 function NoteList() {
     const { noteListId } = useParams()
-    console.log(noteListId);
+    const [noteList, setNoteList] = useState(null);
 
-    return <h1>Notelist</h1>;
+
+    useEffect(() => {
+        const abortController = new AbortController();
+
+        api.fetchNoteList(abortController.signal, noteListId)
+            .then(noteList => setNoteList(noteList));
+        return () => abortController.abort();
+    }, [noteListId])
+
+
+
+    return <div>
+        <h1>{noteList?.name ?? "Loading..."}</h1>
+        <ul>
+            {noteList?.notes?.map(note => <li key={note.id}>{note.text}</li>)}
+        </ul>
+        <Link to={"/"}>All lists</Link>
+    </div>;
 }
 
 export default NoteList;
--- src/main/js/api.js
+++ src/main/js/api.js
@@ -1,11 +1,19 @@
 const endpoints = {
-  notesLists: "http://localhost:8080/lists"
+  notesLists: "http://localhost:8080/lists",
+  noteList: "http://localhost:8080/lists/:noteListId"
 };
 
 function fetchNoteLists(signal) {
-  return fetch(endpoints.notesLists, { signal: signal }).then(response => response.json());
+  return fetch(endpoints.notesLists, { signal: signal })
+      .then(response => response?.json());
+}
+
+function fetchNoteList(signal, noteListId) {
+  return fetch(endpoints.noteList.replace(":noteListId", noteListId), { signal: signal })
+      .then(response => response?.json());
 }
 
 export default {
   fetchNoteLists,
+  fetchNoteList
 };
