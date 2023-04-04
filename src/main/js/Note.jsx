15162951cb06541fd46ac8ea1d6f4739de0029a0
Mark Strempel <mark.strempel@codecool.com>
2023-04-04 10:40:17 +0200

    Added note component.

---
+++ src/main/js/Note.jsx
@@ -0,0 +1,3 @@
+export default function Note({ note }) {
+    return <>{note.text}</>
+}
--- src/main/js/NoteList.jsx
+++ src/main/js/NoteList.jsx
@@ -2,6 +2,7 @@ import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 
 import api from "./api.js";
+import Note from "./Note.jsx";
 
 function NoteList() {
     const { noteListId } = useParams()
@@ -21,7 +22,7 @@ function NoteList() {
     return <div>
         <h1>{noteList?.name ?? "Loading..."}</h1>
         <ul>
-            {noteList?.notes?.map(note => <li key={note.id}>{note.text}</li>)}
+            {noteList?.notes?.map(note => <li key={note.id}><Note note={note}></Note></li>)}
         </ul>
         <Link to={"/"}>All lists</Link>
     </div>;
